e43ea17c86e2d4a8bbc1188e61b37d95ca094a7f
Gideon Oba <deyonoba@gmail.com>
2023-07-24 16:07:17 -0700

    modified 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,11 +13,11 @@ void print_rev(char *s)
 
 	for (i = (strlen(s) - 1); i >= limit; i--)
 	{
-		_putchar(s[i]);
-		if (i == limit)
+		if (s[i] == '\0')
 		{
 			break;
 		}
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
