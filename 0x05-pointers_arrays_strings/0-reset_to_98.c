42b3a333abe6d68108e515da3f3770a8c8417528
Gideon Oba <deyonoba@gmail.com>
2023-07-24 12:51:53 -0700

    Add files used to create 0-98file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value it point to 98
+ *
+ * @n: Takes the address of a stored integer in memory
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
