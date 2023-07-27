9436bd0262357cbd66d592b00ad85abd49706021
Gideon Oba <deyonoba@gmail.com>
2023-07-27 15:32:04 -0700

    Add 0-strcat files to 0x06

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - Concatenate two strings
+ *
+ * @dest: Takes in a string character
+ * @src: Takes in a string character
+ * Return: A concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
