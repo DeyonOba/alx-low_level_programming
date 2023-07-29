185cbfdd2449ac8701c0b4e9ba30c4db5a3f33f3
Gideon Oba <deyonoba@gmail.com>
2023-07-29 16:58:29 -0700

    add 102-print_comb5 file to 0x01 dir

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Print the possible combination of four digits
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i, j, y, z;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (y = '0'; y <= '9'; y++)
+			{
+				for (z = '0'; z <= '9'; z++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(y);
+					putchar(z);
+					if (i == '9' && j == '9' && y == '9' && z == '9')
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
