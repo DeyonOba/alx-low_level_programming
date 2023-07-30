daa85695a3b2e3342edf7ee54b8c65d0f56dff73
Gideon Oba <deyonoba@gmail.com>
2023-07-29 17:16:49 -0700

    modifier 102-print_comb5 to pass checker

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,18 +11,22 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = '0'; j <= '8'; j++)
 		{
 			for (y = '0'; y <= '9'; y++)
 			{
-				for (z = '1'; z <= '9'; z++)
+				for (z = '0'; z <= '9'; z++)
 				{
+					if (i == '0' && j == '0' && y == '0' && z == '0')
+					{
+						continue;
+					}
 					putchar(i);
 					putchar(j);
 					putchar(' ');
 					putchar(y);
 					putchar(z);
-					if (i == '9' && j == '9' && y == '9' && z == '9')
+					if (i == '9' && j == '8' && y == '9' && z == '9')
 					{
 						break;
 					}
