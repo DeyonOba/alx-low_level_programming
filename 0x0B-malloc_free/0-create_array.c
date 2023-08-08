c34677be07c33a61b8c9c703f350ea951f9881d7
Gideon Oba <deyon@gmail.com>
2023-08-08 13:41:48 +0000

    add 0-create_array to 0x0B dir

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create an array of a defined size with a constant character
+ * @size: the size of the array
+ * @c: character constant duplicated in the array
+ *
+ * Return: pointer to an array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *m;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	m = (char *)malloc(sizeof(c) * size);
+	for (i = 0; i < size; i++)
+	{
+		m[i] = c;
+	}
+	return (m);
+}
