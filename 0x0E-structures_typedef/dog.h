4ffe0a1f41f0433d5f5b00dc99b254dec328c4b6
Gideon Oba <deyon@gmail.com>
2023-08-14 13:28:28 +0000

    add dog.h header file to 0x0E

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define a new type structure for dogs
+ * @name: the name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
