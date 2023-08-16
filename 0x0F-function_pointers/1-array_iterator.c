e7ec9ed7842c117667f74b6cc14e85897d942f40
Gideon Oba <deyon@gmail.com>
2023-08-16 19:53:39 +0000

    update code to betty style

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,11 +1,21 @@
 #include "function_pointers.h"
 
+/**
+ * array_iterator - execute function that prints array elements
+ *
+ * @array: pointer to an integer array
+ * @size: size of array
+ * @action: function pointer to be executed
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
+	if (array && size)
 	{
-		(*action)(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			(*action)(array[i]);
+		}
 	}
 }
