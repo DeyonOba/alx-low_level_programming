7ff68c78182776151b71c0ecffa96d5e76726398
Gideon Oba <deyon@gmail.com>
2023-08-20 20:00:32 +0000

    add 0-sum_them_all file to 0x10

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * sum_them_all - sum all its parameters
+ * n: number of parameters
+ *
+ * Return: the sum of parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+
+	va_list args;
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+}
