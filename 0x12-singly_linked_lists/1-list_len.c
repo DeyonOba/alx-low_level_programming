1b2c306d851223d3babe902e447e38f4b7c12f2d
Gideon Oba <deyon@gmail.com>
2023-08-24 23:30:40 +0000

    add 1-list_len.c to 0x12

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "list.h"
+
+/**
+ * list_len - get the number of elements in a linked list
+ *
+ * @h: pointer to the head of the linked list
+ * Return: Number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t num_elem = 0;
+
+	while (h != NULL)
+	{
+		num_elem += 1;
+		h = h->next;
+	}
+	return (num_elem);
+}
