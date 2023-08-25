085a9eb2b0e9ea8129898bfe3ce44e73ed7a7f96
Gideon Oba <deyon@gmail.com>
2023-08-25 13:25:39 +0000

    betty code and style 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,15 +6,16 @@
  * print_list - Gets the number of element in a linked list
  *
  * @h: pointer to the head of the link list
- * Return: The number of element in the linked list\
+ * Return: The number of element in the linked list
  */
 size_t print_list(const list_t *h)
 {
 	size_t num_elem = 0;
-	
+
 	while (h != NULL)
 	{
 		num_elem++;
+
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
@@ -25,3 +26,4 @@ size_t print_list(const list_t *h)
 	}
 	return (num_elem);
 }
+
