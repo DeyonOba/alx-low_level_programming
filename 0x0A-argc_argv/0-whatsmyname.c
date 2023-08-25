c1298a2a41bd081aebd7a033ca81f041d3725ca5
Gideon Oba <deyon@gmail.com>
2023-08-25 23:35:06 +0000

    betty cod and style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @argv: Argument vector contain a string list of command line argument
  * Return: 0 (Success), -1 (Error)
  */
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
 	return (0);
