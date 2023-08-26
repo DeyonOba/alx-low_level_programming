9643fbe6ad93667386548c91915546f627eaff4b
Gideon Oba <deyon@gmail.com>
2023-08-26 16:05:44 +0000

    add 1-args.c to 0x0A

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print number of argument pass in the command line
+ *
+ * @argc: Argument count
+ * Return: 0 (Success), -1 (Error)
+ */
+int main(int argc)
+{
+	printf("%d\n", argc);
+	return (0);
+}
+
