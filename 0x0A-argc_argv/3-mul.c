d991a11a7fa92a0eb700699e4821ca5706f11286
Gideon Oba <deyon@gmail.com>
2023-08-26 16:59:51 +0000

    betty code style

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,16 +4,17 @@
 /**
  * main - Multiple only the first two command line argument
  *
- * @arg: Arguement count
+ * @argc: Arguement count
  * @argv: Arguement vector
  * Return: 0 (Success), 1 (Error)
  */
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
 	if (argc - 1 == 2)
 	{
 		int num1 = atoi(argv[1]);
 		int num2 = atoi(argv[2]);
+
 		printf("%d\n", num1 * num2);
 		return (0);
 	}
