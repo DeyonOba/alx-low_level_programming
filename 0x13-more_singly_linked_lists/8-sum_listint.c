88b5e5682a4bfb0626d8ecd8b24571d5b89a8971
Deyonoba <deyonoba@gmail.com>
2023-08-29 22:58:39 +0000

    add 8-sum_listint.c to 0x13

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Sum of all data (n) of a listint_t linked list
+ *
+ * @head: head pointer
+ * @Return: Sum of data n in nodes
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
