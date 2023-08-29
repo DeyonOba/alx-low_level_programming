5911febc184573d6cc35f4a85ebd31eddc880530
Deyonoba <deyonoba@gmail.com>
2023-08-29 23:34:55 +0000

    refactor: 8-sum_listint to betty code style

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,7 +4,7 @@
  * sum_listint - Sum of all data (n) of a listint_t linked list
  *
  * @head: head pointer
- * @Return: Sum of data n in nodes
+ * Return: Sum of data n in nodes
  */
 int sum_listint(listint_t *head)
 {
