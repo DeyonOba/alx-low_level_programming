766fa2baa4cc401eb3f0a927e13faa871312dc8c
Deyonoba <deyonoba@gmail.com>
2023-08-29 14:59:06 +0000

    add 5-free_listint2.c to 0x13

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Free up a linked list and set head to NULL
+ *
+ * @head: Head pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+
+	while (current != NULL)
+	{
+		listint_t *temp = current;
+
+		current = current->next;
+		free(temp);
+	}
+	*head = NULL;
+}
