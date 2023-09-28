674c12679f00b25d1edec6786d5a848d4f4dc112
Deyonoba <deyonoba@gmail.com>
2023-09-28 11:43:05 +0000

    add main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string.h>
+#include <stdio.h>
+
+int _putchar(char c);
+void print_binary(unsigned long int n);
+unsigned int binary_to_uint(const char *b);
+#endif
