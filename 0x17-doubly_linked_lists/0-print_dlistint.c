5d5090b9536e3d777bfbd0e8cc605bbbf7cb0e0c
Gideon Oba <deyonoba@gmail.com>
2023-12-07 15:46:51 +0000

    modified: 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,16 +10,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t n = 0;
 
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		n += 1;
 		printf("%d\n", h->n);
 		h = h->next;
-		if (h->next == NULL)
-		{
-			printf("%d\n", h->n);
-			n += 1;
-		}
 	}
 
 	return (n);
